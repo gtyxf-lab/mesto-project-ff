c419152785c485d436e63829bdf01573a168c3bf
gtyxf <il>
2025-05-27 18:49:54 +0400

    release vers

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardPlace = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function addCard (cardData, deleteCard) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
 
+  card.querySelector('.card__image').src = cardData.link;
+  card.querySelector('.card__title').textContent = cardData.name;
+  card.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  return card;
+};
 // @todo: Функция удаления карточки
+function deleteCard (evt) {
+  evt.target.closest('.card').remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (card) {
+  const newCard = addCard(card, deleteCard);
+  cardPlace.append(newCard);
+});
